04116fe5d51e819c55219fd2b1a076ee36514b16
wapj <andy@kakaopage.com>
2022-02-13 23:49:14 +0900

    [4장 3절] package.json 파일을 만들고 테스트 해보자 .

---
+++ chapter4/sample-project/index.js
@@ -0,0 +1,8 @@
+console.log("require로 부르면 실행됩니다.");
+
+module.exports = {
+  add: (a, b) => a + b,
+  sub: (a, b) => a - b,
+  multi: (a, b) => a * b,
+  div: (a, b) => a / b,
+};
---
+++ chapter4/sample-test/index.js
@@ -0,0 +1,9 @@
+const calc = require("sample-project");
+
+const a = 17;
+const b = 3;
+
+console.log("a + b = ", calc.add(a, b));
+console.log("a - b = ", calc.sub(a, b));
+console.log("a * b = ", calc.multi(a, b));
+console.log("a / b = ", calc.div(a, b));
