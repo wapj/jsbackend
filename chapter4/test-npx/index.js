5e45ebfecc7a45ac74a70dc9cfd650cdffabdfdc
wapj <andy@kakaopage.com>
2022-02-22 10:27:00 +0900

    [4.5.2] npx 관련 추가

--- chapter4/test-npx/index.js
+++ chapter4/test-npx/index.js
@@ -1,14 +1,6 @@
-
-
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+  /* 주석도 포매팅 해줍니다. */
+  return Math.floor(Math.random() * (max - min)) + min;
 }
-
-
-
-
-
-
-
 
 console.log(getRandomInt(10, 20));
