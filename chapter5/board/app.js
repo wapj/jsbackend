ff7009d96f390f9ae4e7fc6afc73b44f59645bb6
andy.sg <andy.sg@kakaoent.com>
2022-03-16 23:00:17 +0900

    [5장 3절] UI화면 만들기

--- chapter5/board/app.js
+++ chapter5/board/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-
 const app = express();
 
 // config
-app.engine("handlebars", handlebars.engine());
+app.engine(
+  "handlebars",
+  handlebars.engine({
+    helper: require("./config/handlebars-helpers"),
+  }),
+);
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
@@ -31,7 +35,20 @@ app.get("/write", (req, res) => {
 });
 
 app.get("/detail", (req, res) => {
-  res.render("detail", { title: "테스트 게시판" });
+  res.render("detail", {
+    boardTitle: "테스트 게시판",
+    title: "안녕하세요",
+    name: "andy.sg",
+    hits: 123123,
+    createdDt: "2022-03-22",
+    content:
+      "안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n안녕하세요~!\nNode.js 책 열심히 썼습니다!\n읽어보시면 후회 안하실 거예요!\n",
+    comments: [
+      { name: "앤디", comment: "하이", createdDt: "2022-03-13" },
+      { name: "앤디", comment: "하이", createdDt: "2022-03-13" },
+      { name: "앤디", comment: "하이", createdDt: "2022-03-13" },
+    ],
+  });
 });
 
 app.listen(3000);
---
+++ chapter5/board/config/handlebars-helpers.js
@@ -0,0 +1,5 @@
+module.exports = {
+  lengthOfList: function (list) {
+    return list.length;
+  },
+};
