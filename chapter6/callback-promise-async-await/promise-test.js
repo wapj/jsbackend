4fdfc4e43895f940f775b17bc079aed3d5a6bd72
andy.sg <andy.sg@kakaoent.com>
2022-06-03 01:53:41 +0900

    [6.1.2] 프라미스 예제 추가

---
+++ chapter6/callback-promise-async-await/promise-test.js
@@ -0,0 +1,42 @@
+const DB = [];
+
+function saveDB(user) {
+  const oldDBSize = DB.length;
+  DB.push(user);
+  console.log(`save ${user.name} to DB`);
+  return new Promise((resolve, reject) => {
+    if (DB.length > oldDBSize) {
+      resolve(user);
+    } else {
+      reject(new Error("Save DB Error!"));
+    }
+  });
+}
+
+function sendEmail(user) {
+  console.log(`email to ${user.email}`);
+  return new Promise((resolve) => {
+    resolve(user);
+  });
+}
+
+function getResult(user) {
+  return new Promise((resolve, reject) => {
+    resolve(`success register ${user.name}`);
+  });
+}
+
+function registerByPromise(user) {
+  const result = saveDB(user).then(sendEmail).then(getResult);
+  // 아직 완료되지 않았으므로 pending 상태로 나옴
+  console.log(result);
+  return result;
+}
+
+const myUser = { email: "andy@test.com", password: "1234", name: "andy" };
+
+const result = registerByPromise(myUser);
+result.then(console.log);
+
+allResult = Promise.all([saveDB(myUser), sendEmail(myUser), getResult(myUser)]);
+allResult.then(console.log);
