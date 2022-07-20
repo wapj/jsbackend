3c748b8885e962cda729f54e9cd50ad62b1125e5
andy.sg <andy.sg@kakaoent.com>
2022-07-21 01:14:45 +0900

    [6장] nest-blog 컨트롤러 추가

---
+++ src/blog.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import e from 'express';
+
+@Controller('blog')
+export class BlogController {
+  @Get()
+  getAllPosts() {
+    console.log('모든 게시글 가져오기');
+  }
+
+  @Post()
+  createPost() {
+    console.log('게시글 작성');
+  }
+
+  @Get('/:id')
+  getPost() {
+    console.log('하나의 게시글 가져오기');
+  }
+
+  @Delete('/:id')
+  deletePost() {
+    console.log('게시글 삭제');
+  }
+
+  @Put('/:id')
+  updatePost() {
+    console.log('게시글 업데이트');
+  }
+}
