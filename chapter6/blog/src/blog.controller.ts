f4bac1278f9c498f08f7882265458873de95985f
andy.sg <andy.sg@kakaoent.com>
2022-07-25 01:05:40 +0900

    [6장] 메모리에 작성하는 블로그

---
+++ chapter6/blog/src/blog.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BlogService } from './blog.service';
+
+@Controller('blog')
+export class BlogController {
+  blogService: BlogService;
+
+  constructor() {
+    this.blogService = new BlogService();
+  }
+
+  @Get()
+  getAllPosts() {
+    return this.blogService.getAllPosts();
+  }
+
+  @Post()
+  createPost(@Body() postDto) {
+    console.log('게시글 작성');
+    this.blogService.createPost(postDto);
+    return 'success';
+  }
+
+  @Get('/:id')
+  getPost(@Param('id') id: string) {
+    console.log('하나의 게시글 가져오기');
+    return this.blogService.getPost(id);
+  }
+
+  @Delete('/:id')
+  deletePost(@Param('id') id: string) {
+    console.log('게시글 삭제');
+    this.blogService.delete(id);
+    return 'success';
+  }
+
+  @Put('/:id')
+  updatePost(@Param('id') id: string, @Body() postDto) {
+    console.log('게시글 업데이트', id, postDto);
+    return this.blogService.updatePost(id, postDto);
+  }
+}
---
+++ chapter6/blog/src/blog.model.ts
@@ -0,0 +1,8 @@
+interface PostDto {
+  id: string;
+  title: string;
+  content: string;
+  name: string;
+  createdDt?: Date;
+  updatedDt?: Date;
+}
