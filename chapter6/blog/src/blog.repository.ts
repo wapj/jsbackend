45518543ea982fe1b43cd58c5c33a9c788b0504a
andy.sg <andy.sg@kakaoent.com>
2022-08-04 01:51:23 +0900

    몽고디비 연동예제추가

--- chapter6/blog/src/blog.repository.ts
+++ chapter6/blog/src/blog.repository.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
 import { readFile, writeFile } from 'fs/promises';
-import { Model } from 'mongoose';
 import { PostDto } from './blog.model';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { Blog, BlogDocument } from './blog.schema';
 
 export interface BlogRepository {
