b317bb41be37d31710b1df1767b576ae94295958
andy.sg <andy.sg@kakaoent.com>
2022-09-29 23:42:17 +0900

    [9장] chapter7을 chapter9로 디렉터리 명변경

---
+++ chapter9/nest-auth-test/src/auth/google.strategy.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google',
+      scope: ['email', 'profile'],
+    });
+  }
+}
