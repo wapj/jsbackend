ba8d9f302758838303c17e5043a575dab9bb29c8
wapj <wapj2000@gmail.com>
2022-12-27 11:25:38 +0900

    수정

--- chapter7/board-tailwind/configs/mongodb-connection.js
+++ chapter7/board-tailwind/configs/mongodb-connection.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://<username>:<password>@<atlas-host>/board";
+const uri = "mongodb+srv://<user>:<pass>@<atlas-host>/board";
 // const uri = "mongodb://localhost:27017";
 
 module.exports = function (callback) {
