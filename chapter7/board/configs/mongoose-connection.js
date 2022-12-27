acfcc731acccdd3819518c0e9661f424be5be45d
wapj <wapj2000@gmail.com>
2022-12-27 11:25:23 +0900

    [mongoose-connection 수정]

--- chapter7/board/configs/mongoose-connection.js
+++ chapter7/board/configs/mongoose-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const uri =
-  "mongodb+srv://mymongo:test1234@cluster0.c4xru.mongodb.net/board?retryWrites=true&w=majority";
+  "mongodb+srv://<user>:<password>@<atlashost>/board?retryWrites=true&w=majority";
 
 module.exports = function () {
   return mongoose.connect(uri, { useNewUrlParser: true });
