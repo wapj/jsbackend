1c577d4481bcec18a17ef721ca30619686dfdff1
wapj2000 <wapj2000@gmail.com>
2023-03-08 05:12:01 +0900

    [9장] package.json 수정

--- chapter9/config-test/src/configs/common.ts
+++ chapter9/config-test/src/configs/common.ts
@@ -2,4 +2,5 @@ export default {
   logLevel: 'info',
   apiVersion: '1.0.0',
   MESSAGE: 'hello',
+  SERVER_PORT: 3000,
 };
--- chapter9/config-test/src/main.ts
+++ chapter9/config-test/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  console.log("SERVER LISTEN : ",configService.get("SERVER_PORT"));
   await app.listen(configService.get("SERVER_PORT"));
 }
 bootstrap();
