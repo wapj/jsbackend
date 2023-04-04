6e9b196d901e7cc3644f21c3bda1a2c81595fe22
andy.sg <andy.sg@kakaoent.com>
2023-04-05 06:53:43 +0900

    [8장] 블로그 예제코드를 메모리, 파일, 몽고DB로 나누어서 각각 올림

--- chapter8/blog-mongodb/src/app.module.ts
+++ chapter8/blog-mongodb/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BlogController } from './blog.controller';
+import { BlogFileRepository, BlogMongoRepository } from './blog.repository';
+import { Blog, BlogSchema } from './blog.schema';
 import { BlogService } from './blog.service';
+
 @Module({
-imports: [],
-controllers: [BlogController],
-providers: [BlogService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://{유저ID}:{password}@{클러스터주소}/blog',
+    ),
+    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+  ],
+  controllers: [BlogController],
+  providers: [BlogService, BlogFileRepository, BlogMongoRepository],
 })
 export class AppModule {}
--- chapter8/blog-mongodb/src/blog.service.ts
+++ chapter8/blog-mongodb/src/blog.service.ts
@@ -1,1 +1,32 @@
-export class BlogService {}
+import { Injectable } from '@nestjs/common';
+import { PostDto } from "./blog.model";
+import { BlogMongoRepository } from "./blog.repository";
+
+@Injectable()
+export class BlogService {
+  
+  constructor(private blogRepository: BlogMongoRepository) {  }
+
+  posts = [];
+
+  async getAllPosts() {
+    console.log(this.blogRepository)
+    return await this.blogRepository.getAllPost();
+  }
+
+  createPost(postDto: PostDto) {
+    this.blogRepository.createPost(postDto);
+  }
+
+  async getPost(id): Promise<PostDto> {
+    return await this.blogRepository.getPost(id);
+  }
+
+  delete(id) {
+    this.blogRepository.deletePost(id);
+  }
+
+  updatePost(id, postDto: PostDto) {
+    this.blogRepository.updatePost(id, postDto);
+  }
+}
