42909cde81e82ed8a801a70dcec6653189c57807
andy.sg <andy.sg@kakaoent.com>
2023-12-15 03:39:05 +0900

    ESM 커스텀 훅 예제 추가

---
+++ nodejs20_major_features/loader.mjs
@@ -0,0 +1,11 @@
+// custom-loader.mjs
+export async function load(url, context, nextLoad) {
+  if (url.startsWith("https://")) {
+    const response = await fetch(url, { redirect: "follow" });
+    const source = await response.text();
+
+    return {source, format: "module", shortCircuit: true}
+  } else {
+    return await nextLoad(url, context)
+  }
+}
