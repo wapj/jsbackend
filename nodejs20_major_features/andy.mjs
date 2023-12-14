30929cc63e451e8eef8cad07c7baedf2f18af7ea
andy.sg <andy.sg@kakaoent.com>
2023-12-15 03:41:10 +0900

    ESM 커스텀 모듈 훅 예제 추가

---
+++ nodejs20_major_features/andy.mjs
@@ -0,0 +1,30 @@
+export async function resolve(specifier, context, defaultResolve) {
+  // 여기에서 사용자 정의 해석 로직을 구현
+  if (specifier.endsWith('.andy')) {
+    return {
+      url: `andy:${specifier}`,
+      shortCircuit: true
+    };
+  }
+
+  return defaultResolve(specifier, context);
+}
+
+export async function load(url, context, defaultLoad) {
+  console.log('load >> ', url);
+  if (url.startsWith('andy:')) {
+    return {
+      format: 'module',
+      source: `
+        // 사용자 정의 모듈 소스
+        export function test() {
+          console.log('Hello Node.JS! I am Andy');
+        }
+      `,
+      shortCircuit: true
+    };
+  }
+
+  // 기본 로드 로직에 위임
+  return defaultLoad(url, context);
+}
---
+++ nodejs20_major_features/main.js
@@ -0,0 +1,6 @@
+import rightPad from 'https://esm.sh/right-pad@1.0.1'
+import {test} from 'test.andy'
+
+console.log(rightPad('foo', 5, '.'))
+
+test()
